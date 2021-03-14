f7db3da602e173201c2203831138bde7ca336f55
Jona <jonathan.vargas1104@gmail.com>
2021-03-14 13:37:06 -0500

    Setup projecr

---
+++ src/redux/slices/counter.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+/*  STATE */
+const initialState = {
+  counter: 0,
+};
+
+const slice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    incrementCounter(state) {
+      state.counter = state.counter + 1;
+    },
+    decrementCounter(state, action) {
+        state.counter = state.counter - 1;
+    },
+    setCounter(state, action) {
+        const newValue = action.payload;
+        state.counter = newValue;
+    },
+  }
+});
+
+export const reducer = slice.reducer;
+
+
+/* ACTIONS */
+export const incrementCounterAction = () => async (dispatch) => {
+  dispatch(slice.actions.increment());
+};
+
+export const incrementCounterAction = () => async (dispatch) => {
+    dispatch(slice.actions.decrementCounter());
+};
+
+export const setCounterAction = (newValue) => async (dispatch) => {
+    dispatch(slice.actions.setCounter(newValue));
+};
+  
